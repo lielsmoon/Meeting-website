ccd8f2278237fdc3b36befad5bafdb3a54eb8970
lielsmoon <baluzaveta@gmail.com>
2022-01-13 18:33:53 +0200

    improved page

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/navBar";
-import { Route } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import UserPages from "./layouts/users";
@@ -9,9 +9,12 @@ function App() {
     return (
         <div>
             <NavBar />
-            <Route path="/login" component={Login} />
-            <Route path="/main" component={Main} />
-            <Route path="/users/:userId?" component={UserPages} />
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/users/:userId?" component={UserPages} />
+                <Route path="/" component={Main} />
+                <Redirect to="/" />
+            </Switch>
         </div>
     );
 }
--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Users from "../components/users";
+import UsersList from "../components/usersList";
 import UserPage from "../components/userPage";
 
 const UserPages = () => {
     const params = useParams();
     const { userId } = params;
-    return <>{userId ? <UserPage id={userId} /> : <Users />}</>;
+    return <>{userId ? <UserPage id={userId} /> : <UsersList />}</>;
 };
 
 export default UserPages;
